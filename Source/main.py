da2a3d69ee2c73b587b3cdd25bd2a81cb255a1aa
frizik <tester251100@gmail.com>
2019-08-07 15:52:29 +0300

    07.08.2019

--- Source/main.py
+++ Source/main.py
@@ -9,22 +9,25 @@ import parser  as pr
 import preparation as pre
 import requests
 
+SeparationString = "=========================================================="
+
 # Функция вывода сообщения
-def WriteMsgFunc(user_id, message,random_id,vk):
+def WriteMsgFunc(user_id, message,vk):
+    random_id = random.randint(0, 12345678900987654321)
     vk.method('messages.send', {'user_id': user_id,'message': message,'random_id':random_id})
 
 # Функция с приветственным сообщением 
-def StartFunc(event,user_id,random_id,vk):
+def StartFunc(event,user_id,vk):
     hello = open("/home/frizik/Projects/ScanTelegramBot/TextMessages/Hello_Massage.txt")
     StringFromCommands = hello.read()
-    WriteMsgFunc(event.user_id,StringFromCommands,random_id,vk)
+    WriteMsgFunc(event.user_id,StringFromCommands,vk)
     hello.close()
 
 # Функция для вывода списка доступных команд
-def HelpFunc(event,user_id,random_id,vk):
+def HelpFunc(event,user_id,vk):
     commands=open("/home/frizik/Projects/ScanTelegramBot/TextMessages/Commands.txt","r")
     StringFromCommands=commands.read()
-    WriteMsgFunc(event.user_id,StringFromCommands,random_id,vk)
+    WriteMsgFunc(event.user_id,StringFromCommands,vk)
     commands.close()
 
 # Подготовительная функция
@@ -32,7 +35,7 @@ def Prepare(Answer,CountOfDash,CountOfZeros,TownName):
     pre.StringParser(Answer,CountOfDash,CountOfZeros,TownName)
 
 #Функция выдающая диапозон ip адрессов на город
-def NewTownFunc(event,user_id,random_id,vk,TownName,login,password):
+def NewTownFunc(event,user_id,vk,TownName,login,password):
     Answer = pr.AutoParserIPs(TownName)
     logPath = "/home/frizik/Projects/ScanTelegramBot/Data/ips.txt"
     ips = open(logPath,"w")
@@ -43,51 +46,52 @@ def NewTownFunc(event,user_id,random_id,vk,TownName,login,password):
     vk_session.auth(token_only=True)
 
     upload = vk_api.VkUpload(vk_session)
-    photo = upload.photo('/home/frizik/Photo/москва/DSC05281.JPG',album_id=264629427,group_id=184430889)
-    vk_photo_url = 'https://vk.com/photo{}_{}'.format(photo[0]['owner_id'], photo[0]['id'])
-    print(photo, '\nLink: ', vk_photo_url)
-    WriteMsgFunc(event.user_id,photo_url,random_id,vk)
-
-    #document = upload.docs('')Загрзука фотографий работает теперь нудно разобраться с документами
-
+    document = upload.document("/home/frizik/Projects/ScanTelegramBot/Data/ips.txt","db",group_id = 184430889)
+    DocumentUrl = "vk.com/doc{}_{}".format(document["doc"]["owner_id"],document["doc"]["id"])
+    WriteMsgFunc(event.user_id,"Получен диапозон ip адрессов города,в этом текстовом файле вы можете их просмотреть",vk)
+    WriteMsgFunc(event.user_id,DocumentUrl,vk)
+    print(colored("Город ","blue"),colored(TownName,"red"),colored(" обработан!!!","blue"))
+    
 # Основная функция 
 def main():
     AsciiArt = pyfiglet.figlet_format("ScanVkBot")
     tmp = sp.call('clear',shell=True)
     print(colored(AsciiArt,"yellow"))
+    print(colored(SeparationString,"magenta"))
     
     print(colored("Введите ключ бота:","yellow"),end = "")
     token = input()
     print(colored("Введите логин от страницы администратора:","yellow"),end = "")
     login = input()
-    print(colored("Введите пароль оут страницы администратораЖ","yellow"),end = "")
+    print(colored("Введите пароль от страницы администратора:","yellow"),end = "")
     password = input()
+    
+    try:
+        vk = vk_api.VkApi(token=token)
+        print(colored("Успешное подключение,бот запущен!!!","green"))
+        print(colored(SeparationString,"magenta"))
+    except:
+        print(colored("Не удалось подключится,проверьте правильность введённых данных","red"))
 
-    token = input()
-    vk = vk_api.VkApi(token=token)
     random.seed(version=2)
     longpoll = VkLongPoll(vk)
     Triger = False
     for event in longpoll.listen():
-        random_id = random.randint(0, 12345678900987654321)
         if event.type == VkEventType.MESSAGE_NEW:
             if event.to_me:
                 request = event.text
                 if request == "старт" or request == "Старт":
-                    StartFunc(event,event.user_id,random_id,vk)
+                    StartFunc(event,event.user_id,vk)
                 if request == "помощь" or request == "Помощь":
-                    HelpFunc(event,event.user_id,random_id,vk)
+                    HelpFunc(event,event.user_id,vk)
                 if request == "город" or request == "Город":
-                    WriteMsgFunc(event.user_id,"Введите название города для скана",random_id,vk)
-                    Triger = True 
-                    print(Triger)
+                    WriteMsgFunc(event.user_id,"Введите название города для скана",vk)
+                    Triger = True
                 else:
                     if Triger == True:
                         Triger = False
-                        NewTownFunc(event,event.user_id,random_id,vk,request,login,password)
-                        
-                    else :
-                        WriteMsgFunc(event.user_id, "Неизветсная команда,проверьте правильность введённых данных",random_id,vk)
+                        WriteMsgFunc(event.user_id,"Подготавливается диапозон адрессов,пожалуйста подождите",vk)
+                        NewTownFunc(event,event.user_id,vk,request,login,password)
 
 # При импорте 
 if __name__ == "__main__":
--- Source/wolker.py
+++ Source/wolker.py
@@ -1,4 +1,11 @@
 #Скрытый функционал
-    '''driver.get_screenshot_as_file('/home/frizik/test.png') 
-    driver.save_screenshot('/home/frizik/test.png')'''
+
+'''driver.get_screenshot_as_file('/home/frizik/test.png') 
+driver.save_screenshot('/home/frizik/test.png')'''
    
+
+'''upload = vk_api.VkUpload(vk_session)
+    photo = upload.photo('/home/frizik/Photo/москва/DSC05281.JPG',album_id=264629427,group_id=184430889)
+    vk_photo_url = 'https://vk.com/photo{}_{}'.format(photo[0]['owner_id'], photo[0]['id'])
+    print(photo, '\nLink: ', vk_photo_url)
+    WriteMsgFunc(event.user_id,photo_url,random_id,vk)'''
